2bee43dafbc34466d5be5f43d7cd3b184a1c8b27
Zachary <53413395+ze-ck@users.noreply.github.com>
2020-04-25 21:39:59 -0400

    list add/remove functionality added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import React from 'react';
-import List from './Components/List';
+import Timer from './Components/Timer'
 import './styles/styles.scss';
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            todos: [{
-                task: 'Go grocery shopping',
-                id: '1',
-                completed: 'yes'
-            },
-            {
-                task: 'Walk the dog',
-                id: '2',
-                completed: 'yes'
-            }
-        ],
+            todos: [],
             todo: {
                 task: '',
-                id: '',
-                completed: ''
+                id: ''
             }
         }
     
         this.handleSubmit = this.handleSubmit.bind(this)
         this.onInputChange = this.onInputChange.bind(this)
+        this.onRemove = this.onRemove.bind(this)
     }
 
     handleSubmit(e) {
@@ -40,17 +31,33 @@ class App extends React.Component {
 
     onInputChange(e) {
         const newTask = e.target.value;
+        const newId = uuidv4();
         this.setState(prevState => {
             let newState = { ...prevState.todo }
             newState.task = newTask;
+            newState.id = newId;
             return { todo: newState }
+        })
+    }
+
+    onRemove(e) {
+        let id = e.target.parentElement.getAttribute("data-key");
+        this.setState(prevState => {
+            // console.log(prevState.todos[0].id);
+            let newState = prevState.todos.filter((el) => {
+                return el.id !== id
+            })
+            return { todos: newState }
         })
     }
 
     render() {
         return (
             <div className="container">
-                <List todos={this.state.todos} />
+                <Timer />
+                <div>
+                    {this.state.todos.map((el) => <li key={el.id} data-key={el.id}>{el.task}<button onClick={this.onRemove}>Remove Task</button></li>)}
+                </div>
                 <form onSubmit={this.handleSubmit}>
                     <input 
                         placeholder="Enter a todo" 
